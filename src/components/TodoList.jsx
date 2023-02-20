f796bad18548923a357a375c6958fd2ed52592f1
HazalGultekin <hazal.gultekin@lab2023.com>
2023-02-20 23:23:40 +0300

    Add and delete files

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const App = () => {
+const TodoList = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ const App = () => {
   )
 }
 
-export default App
+export default TodoList
