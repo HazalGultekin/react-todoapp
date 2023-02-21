bb74fc1c8afc690e3e034b91014d7a1eb49931cb
HazalGultekin <hazal.gultekin@lab2023.com>
2023-02-21 22:36:16 +0300

    Add reducer.js and TodoContext.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { TodoLayer } from './context/TodoContext';
+import reducer, { initialState } from './context/reducer';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <TodoLayer initialState={initialState} reducer={reducer}>
+      <App></App>
+    </TodoLayer>
   </React.StrictMode>
-);
-
+)
+document.getElementById('root')
