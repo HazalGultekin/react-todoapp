8ca4c7c983f921147737985e95d12dbc73d125d4
HazalGultekin <hazal.gultekin@lab2023.com>
2023-02-22 21:29:18 +0300

    Add components and context files

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { TodoLayer } from './context/TodoContext';
 import reducer, { initialState } from './context/reducer';
 
-
 ReactDOM.render(
   <React.StrictMode>
     <TodoLayer initialState={initialState} reducer={reducer}>
-      <App></App>
+      <App />
     </TodoLayer>
-  </React.StrictMode>
-)
-document.getElementById('root')
+  </React.StrictMode>,
+  document.getElementById('root')
+);
